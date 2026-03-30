09d853252b55e842a865660d0ee82c50945c0752
magikatto <nguyenminhtptn1111@gmail.com>
2026-03-31 05:15:15 +0700

    feat(exception): add custom auction exception hierarchy

---
+++ auction-server/src/main/java/com/auction/server/model/exception/AuctionClosedException.java
@@ -0,0 +1,7 @@
+package com.auction.server.model.exception;
+// Được throw khi đặt giá cho một auction không ở trạng thái RUNNING
+public class AuctionClosedException extends AuctionException {
+    public AuctionClosedException(String message) {
+        super(message);
+    }
+}
