8b08beaaa6b63251a9316cdafec37ebe24cfbf3b
magikatto <nguyenminhtptn1111@gmail.com>
2026-03-31 11:33:10 +0700

    feat(exception): add custom auction exception hierarchy

---
+++ auction-server/src/main/java/com/auction/server/model/exception/AuctionException.java
@@ -0,0 +1,10 @@
+package com.auction.server.model.exception;
+
+public class AuctionException extends RuntimeException {
+    public AuctionException(String message) {
+        super(message);
+    }
+    public AuctionException(String message, Throwable cause) {
+        super(message,cause);
+    }
+}
